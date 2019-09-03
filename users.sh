da0284e87e7dd1d13e5ac3f28a6dde1ed9ab0e4a
jay aljoe <nenojaafar3@gmail.com>
2019-09-02 21:25:16 -0500

    you were missing the " " around $3 but
    you were missing the " " around $3 but everything else is awesome

--- users.sh
+++ users.sh
@@ -6,7 +6,7 @@ PASSWORD=$3
 addUser()
 {
   # Checks to see is Password is empty. If so, it generates a random password
-  if [ -z $3 ];
+  if [ -z "$3" ];
   then
     PASSWORD=$(date +%s | sha256sum | base64 | head -c 32)
   fi
